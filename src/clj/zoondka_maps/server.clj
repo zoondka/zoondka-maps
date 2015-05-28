d144dfdaffaf5fd804ce0a472ba39800edfe3125
Lucas Leblow <leblowl@gmail.com>
2015-05-27 21:28:49 -0700

    changed default port

--- src/clj/zoondka_maps/server.clj
+++ src/clj/zoondka_maps/server.clj
@@ -5,7 +5,7 @@
 
 (defn run [handler & [port]]
   (defonce ^:private server
-    (let [port (Integer. (or port 10333))]
+    (let [port (Integer. (or port 8090))]
       (print "Starting web server on port " port ".\n")
       (run-server handler {:port port})))
   server)
