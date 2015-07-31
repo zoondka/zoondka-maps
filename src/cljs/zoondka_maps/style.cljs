6a04851cca24333a7e4019026174c76c0976b846
Lucas Leblow <leblowl@gmail.com>
2015-07-31 16:06:09 -0700

    hooked up to new zoondka tile-srv endpoint

--- src/cljs/zoondka_maps/style.cljs
+++ src/cljs/zoondka_maps/style.cljs
@@ -21,70 +21,70 @@
                "@admin_l2_width" {:stops [[2 0.6] [20 6]]}
                "@admin_l3_width" {:stops [[5 0.6] [20 6]]}}
 
-   :sources {:mb {:type "vector"
-                  :url "mapbox://mapbox.mapbox-terrain-v2,mapbox.mapbox-streets-v6"}
-             :osm {:type "vector"
-                   :url "http://tile.openstreetmap.us/vectiles-highroad/{z}/{x}/{y}.mvt"}}
+   :sources {:tile-gen {:type "vector"
+                        :tiles ["https://maps.zoondka.com/tile-srv/all/{z}/{x}/{y}.mvt"]}
+             :highroad {:type "vector"
+                        :tiles ["http://tile.openstreetmap.us/vectiles-highroad/{z}/{x}/{y}.mvt"]}}
    :layers [{:id "background"
              :type "background"
              :paint {:background-color "@land"}}
             {:id "landcover_snow"
              :type "fill"
-             :source "mb"
+             :source "tile-gen"
              :source-layer "landcover"
              :filter ["==" "class" "snow"]
              :paint {:fill-color "@snow"}}
             {:id "landcover_crop"
              :type "fill"
-             :source "mb"
+             :source "tile-gen"
              :source-layer "landcover"
              :filter ["==" "class" "crop"]
              :paint {:fill-color "@crop"}}
             {:id "landcover_grass"
              :type "fill"
-             :source "mb"
+             :source "tile-gen"
              :source-layer "landcover"
              :filter ["==" "class" "grass"]
              :paint {:fill-color "@grass"
                      :fill-opacity {:stops [[12 1] [16 0.2]]}}}
             {:id "landcover_scrub"
              :type "fill"
-             :source "mb"
+             :source "tile-gen"
              :source-layer "landcover"
              :filter ["==" "class" "scrub"]
              :paint {:fill-color "@scrub"
                      :fill-opacity {:stops [[12 1] [16 0.2]]}}}
             {:id "landcover_wood"
              :type "fill"
-             :source "mb"
+             :source "tile-gen"
              :source-layer "landcover"
              :filter ["==" "class" "wood"]
              :paint {:fill-color "@wood"
                      :fill-opacity {:stops [[12 1] [16 0.2]]}}}
             {:id "landuse_wood"
              :type "fill"
-             :source "mb"
+             :source "tile-gen"
              :source-layer "landuse"
              :filter ["==" "class" "wood"]
              :paint {:fill-color "rgba(0, 182, 0, 0.1)"
                      :fill-outline-color "rgba(0, 182, 0, 0.2)"}}
             {:id "landuse_rock"
              :type "fill"
-             :source "mb"
+             :source "tile-gen"
              :source-layer "landuse"
              :filter ["==" "class" "rock"]
              :paint {:fill-color "rgba(77, 77, 77, 0.1)"
                      :fill-outline-color "rgba(77, 77, 77, 0.2)"}}
             {:id "landuse_agriculture"
              :type "fill"
-             :source "mb"
+             :source "tile-gen"
              :source-layer "landuse"
              :filter ["==" "class" "agriculture"]
              :paint {:fill-color "rgba(177, 177, 0, 0.1)"
                      :fill-outline-color "rgba(177, 177, 0, 0.1)"}}
             {:id "road_main_casing"
              :type "line"
-             :source "mb"
+             :source "tile-gen"
              :source-layer "road"
              :filter ["==" "class" "main"]
              :layout {:line-join "round"
@@ -95,7 +95,7 @@
                      :line-opacity {:stops [[8 0] [9 1]]}}}
             {:id "road_motorway_casing"
              :type "line"
-             :source "osm"
+             :source "tile-gen"
              :layout {:line-join "round"
                       :line-cap "round"}
              :paint {:line-color "#ff00ff"
@@ -113,13 +113,13 @@
                      :line-opacity {:stops [[3 0] [4 1]]}}}
             {:id "water"
              :type "fill"
-             :source "mb"
+             :source "tile-gen"
              :source-layer "water"
              :paint {:fill-color "@water"
                      :fill-outline-color "#a2bdc0"}}
             {:id "contour_line"
              :type "line"
-             :source "mb"
+             :source "tile-gen"
              :source-layer "contour"
              :filter ["!=" "index" 5]
              :paint {:line-color "#000"
@@ -127,7 +127,7 @@
                      :line-opacity {:stops [[11 0.05] [12 0.1]]}}}
             {:id "contour_line_loud"
              :type "line"
-             :source "mb"
+             :source "tile-gen"
              :source-layer "contour"
              :filter ["==" "index" 5]
              :paint {:line-color "#000"
@@ -135,7 +135,7 @@
                      :line-opacity {:stops [[11 0.1] [12 0.2]]}}}
             {:id "admin_l2"
              :type "line"
-             :source "mb"
+             :source "tile-gen"
              :source-layer "admin"
              :filter ["==" "admin_level" 2]
              :layout {:line-join "round"
@@ -144,7 +144,7 @@
                      :line-width "@admin_l2_width"}}
             {:id "admin_l3"
              :type "line"
-             :source "mb"
+             :source "tile-gen"
              :source-layer "admin"
              :filter ["in" "admin_level" 3 4 5]
              :layout {:line-join "round"}
