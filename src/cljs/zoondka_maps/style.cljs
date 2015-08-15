4d4c8fdc5510894567d87a3507d51efefdf9bb9d
Lucas Leblow <leblowl@gmail.com>
2015-08-14 17:25:40 -0700

    using home grown vector tile source

--- src/cljs/zoondka_maps/style.cljs
+++ src/cljs/zoondka_maps/style.cljs
@@ -21,134 +21,20 @@
                "@admin_l2_width" {:stops [[2 0.6] [20 6]]}
                "@admin_l3_width" {:stops [[5 0.6] [20 6]]}}
 
-   :sources {:tile-gen {:type "vector"
-                        :tiles ["https://maps.zoondka.com/tile-srv/all/{z}/{x}/{y}.mvt"]}
-             :highroad {:type "vector"
-                        :tiles ["http://tile.openstreetmap.us/vectiles-highroad/{z}/{x}/{y}.mvt"]}}
+   :sources {:mapbox   {:type "vector"
+                        :url "mapbox://mapbox.mapbox-streets-v5"}
+             :tile-gen {:type "vector"
+                        :tiles ["https://maps.zoondka.com/tile-srv/all/{z}/{x}/{y}.mvt"]}}
    :layers [{:id "background"
              :type "background"
              :paint {:background-color "@land"}}
-            {:id "landcover_snow"
-             :type "fill"
-             :source "tile-gen"
-             :source-layer "landcover"
-             :filter ["==" "class" "snow"]
-             :paint {:fill-color "@snow"}}
-            {:id "landcover_crop"
-             :type "fill"
-             :source "tile-gen"
-             :source-layer "landcover"
-             :filter ["==" "class" "crop"]
-             :paint {:fill-color "@crop"}}
-            {:id "landcover_grass"
-             :type "fill"
-             :source "tile-gen"
-             :source-layer "landcover"
-             :filter ["==" "class" "grass"]
-             :paint {:fill-color "@grass"
-                     :fill-opacity {:stops [[12 1] [16 0.2]]}}}
-            {:id "landcover_scrub"
-             :type "fill"
-             :source "tile-gen"
-             :source-layer "landcover"
-             :filter ["==" "class" "scrub"]
-             :paint {:fill-color "@scrub"
-                     :fill-opacity {:stops [[12 1] [16 0.2]]}}}
-            {:id "landcover_wood"
-             :type "fill"
-             :source "tile-gen"
-             :source-layer "landcover"
-             :filter ["==" "class" "wood"]
-             :paint {:fill-color "@wood"
-                     :fill-opacity {:stops [[12 1] [16 0.2]]}}}
-            {:id "landuse_wood"
-             :type "fill"
-             :source "tile-gen"
-             :source-layer "landuse"
-             :filter ["==" "class" "wood"]
-             :paint {:fill-color "rgba(0, 182, 0, 0.1)"
-                     :fill-outline-color "rgba(0, 182, 0, 0.2)"}}
-            {:id "landuse_rock"
-             :type "fill"
-             :source "tile-gen"
-             :source-layer "landuse"
-             :filter ["==" "class" "rock"]
-             :paint {:fill-color "rgba(77, 77, 77, 0.1)"
-                     :fill-outline-color "rgba(77, 77, 77, 0.2)"}}
-            {:id "landuse_agriculture"
-             :type "fill"
-             :source "tile-gen"
-             :source-layer "landuse"
-             :filter ["==" "class" "agriculture"]
-             :paint {:fill-color "rgba(177, 177, 0, 0.1)"
-                     :fill-outline-color "rgba(177, 177, 0, 0.1)"}}
-            {:id "road_main_casing"
-             :type "line"
-             :source "tile-gen"
-             :source-layer "road"
-             :filter ["==" "class" "main"]
-             :layout {:line-join "round"
-                      :line-cap "round"}
-             :paint {:line-color "#fff"
-                     :line-width "@main_casing_width"
-                     :line-gap-width "@main_width"
-                     :line-opacity {:stops [[8 0] [9 1]]}}}
-            {:id "road_motorway_casing"
-             :type "line"
-             :source "tile-gen"
-             :layout {:line-join "round"
-                      :line-cap "round"}
-             :paint {:line-color "#ff00ff"
-                     :line-width "@motorway_casing_width"
-                     :line-gap-width "@motorway_width"}}
-            {:id "road_main"
-             :ref "road_main_casing"
-             :paint {:line-color "#000"
-                     :line-width "@main_width"
-                     :line-opacity {:stops [[3 0] [4 1]]}}}
-            {:id "road_motorway"
-             :ref "road_motorway_casing"
-             :paint {:line-color "#777"
-                     :line-width "@motorway_width"
-                     :line-opacity {:stops [[3 0] [4 1]]}}}
             {:id "water"
              :type "fill"
              :source "tile-gen"
              :source-layer "water"
-             :paint {:fill-color "@water"
-                     :fill-outline-color "#a2bdc0"}}
-            {:id "contour_line"
-             :type "line"
+             :paint {:fill-color "@water"}}
+            {:id "earth"
+             :type "fill"
              :source "tile-gen"
-             :source-layer "contour"
-             :filter ["!=" "index" 5]
-             :paint {:line-color "#000"
-                     :line-width 1.2
-                     :line-opacity {:stops [[11 0.05] [12 0.1]]}}}
-            {:id "contour_line_loud"
-             :type "line"
-             :source "tile-gen"
-             :source-layer "contour"
-             :filter ["==" "index" 5]
-             :paint {:line-color "#000"
-                     :line-width 1.2
-                     :line-opacity {:stops [[11 0.1] [12 0.2]]}}}
-            {:id "admin_l2"
-             :type "line"
-             :source "tile-gen"
-             :source-layer "admin"
-             :filter ["==" "admin_level" 2]
-             :layout {:line-join "round"
-                      :line-cap "round"}
-             :paint {:line-color "#999999"
-                     :line-width "@admin_l2_width"}}
-            {:id "admin_l3"
-             :type "line"
-             :source "tile-gen"
-             :source-layer "admin"
-             :filter ["in" "admin_level" 3 4 5]
-             :layout {:line-join "round"}
-             :paint {:line-color "#999999"
-                     :line-dasharray [2 1]
-                     :line-opacity {:stops [[0 0] [4 1]]}
-                     :line-width "@admin_l3_width"}}]})
+             :source-layer "earth"
+             :paint {:fill-color "@land"}}]})
